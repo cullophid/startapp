a1fd2ea30a738c845bf454f7171eaad5c4b62454
Andreas Møller <andreas.moller@gmail.com>
2017-02-03 14:22:16 +0000

    added hooks

--- build/startapp.js
+++ build/startapp.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -17,19 +17,30 @@ exports.default = function (_ref) {
       view = _ref.view,
       update = _ref.update,
       _ref$subs = _ref.subs,
-      subs = _ref$subs === undefined ? [] : _ref$subs;
+      subs = _ref$subs === undefined ? [] : _ref$subs,
+      _ref$hooks = _ref.hooks,
+      hooks = _ref$hooks === undefined ? {} : _ref$hooks;
 
   var state = model;
   var msg = void 0;
+  var callHook = function callHook(hook) {
+    for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+      args[_key - 1] = arguments[_key];
+    }
+
+    return hooks[hook] && hooks[hook].apply(hooks, args);
+  };
 
   var dispatch = function dispatch(action) {
     return function () {
-      for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-        args[_key] = arguments[_key];
+      for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+        args[_key2] = arguments[_key2];
       }
 
+      callHook.apply(undefined, ['onAction', action].concat(args));
       var next = update[action].apply(update, [state].concat(args, [msg]));
       if (!Array.isArray(next)) throw new Error('Update must return a tuple');
+      callHook.apply(undefined, ['onUpdate', state].concat(args));
       state = next[0];
 
       if (next[1]) {
--- src/startapp.js
+++ src/startapp.js
@@ -3,13 +3,17 @@
 const merge = (a, b) => Object.assign({}, a, b)
 
 
-export default ({model, view, update, subs = []}) => {
+export default ({model, view, update, subs = [], hooks = {}}) => {
   let state = model
   let msg
+  const callHook = (hook, ...args) =>
+    hooks[hook] && hooks[hook](...args)
 
   const dispatch = action => (...args) => {
+    callHook('onAction', action, ...args)
     const next = update[action](state, ...args, msg)
     if (!Array.isArray(next)) throw new Error('Update must return a tuple')
+    callHook('onUpdate', state, ...args)
     state = next[0]
 
     if (next[1]) {
